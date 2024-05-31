ff85afe5f2c56bb69e99c4b7168cb2f46769e728
Pandamunchies <163986177+Pandamunchies@users.noreply.github.com>
2024-05-31 03:53:57 -0400

    create computer choice logic

--- js/game.js
+++ js/game.js
@@ -1,1 +1,18 @@
-console.log("hello?");
+function getRandomInt() {
+    return Math.floor(Math.random()*3+1);
+}
+let randomNumber = getRandomInt(); 
+    console.log(randomNumber); 
+let computerPick= convertPick(randomNumber)
+function convertPick(number){
+    if(number===1){
+    return "rock";
+    }
+    else if(number===2){
+    return "paper";
+    }
+    else if(randomNumber===3){
+    return  "scissor";
+    }
+}
+console.log(computerPick);
